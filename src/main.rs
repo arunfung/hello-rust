5404ee7b26da2a94ad3a4e51aa61d1fa3d345809
Arun Fung <arunfung@gmail.com>
2022-09-12 21:38:07 +0800

    数据结构

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,10 @@ fn main() {
     // function_parameter();
 
     // 分号;表示其返回值为 unit
-    returned_value();
+    // returned_value();
+
+    // 聊天服务数据结构
+    chat_room();
 }
 
 // 1、猜测数字的小游戏
@@ -99,4 +102,52 @@ fn pi() -> f64 {
 
 fn not_pi() {
     3.1415926;
+}
+
+
+// 聊天服务数据结构
+#[derive(Debug)]
+enum Gender {
+    Unspecified = 0,
+    Female = 1,
+    Male = 2,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct UserId(u64);
+
+#[derive(Debug)]
+struct User {
+    id: UserId,
+    name: String,
+    gender: Gender,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct TopicId(u64);
+
+#[derive(Debug)]
+struct Topic {
+    id: TopicId,
+    name: String,
+    owner: UserId,
+}
+
+// 聊天室中的事件
+#[derive(Debug)]
+enum Event {
+    Join((UserId, TopicId)),
+    Leave((UserId, TopicId)),
+    Message((UserId, TopicId, String)),
+}
+
+fn chat_room() {
+    let alice = User { id: UserId(1), name: "Alice".into(), gender: Gender::Female };
+    let bob = User { id: UserId(2), name: "Bob".into(), gender: Gender::Male };
+    let topic = Topic { id: TopicId(1), name: "rust".into(), owner: UserId(1) };
+    let event1 = Event::Join((alice.id, topic.id));
+    let event2 = Event::Join((bob.id, topic.id));
+    let event3 = Event::Message((alice.id, topic.id, "Hello world!".into()));
+
+    println!("event1: {:?}, event2: {:?}, event3: {:?}", event1, event2, event3);
 }
