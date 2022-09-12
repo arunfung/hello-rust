806c8cc38e3b68919c8c61ecb2c96b104aa46f4a
Arun Fung <arunfung@gmail.com>
2022-09-12 17:46:33 +0800

    函数传参

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,28 @@ fn main() {
     // }
 
     // 2、请求一个url并将获取的HTML转成md
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
-    println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
-    println!("Converting html to markdown...");
-    let md = html2md::parse_html(&body);
-    fs::write(output, md.as_bytes()).unwrap();
+    // let url = "https://www.rust-lang.org/";
+    // let output = "rust.md";
+    // println!("Fetching url: {}", url);
+    // let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    // println!("Converting html to markdown...");
+    // let md = html2md::parse_html(&body);
+    // fs::write(output, md.as_bytes()).unwrap();
 
+    // 3、函数传参
+    println!("apply square: {}", apply(2, square));
+    println!("apply cube: {}", apply(2, cube));
+
+}
+
+fn apply(value: i32, f:fn(i32) -> i32) -> i32 {
+    f(value)
+}
+
+fn square(value: i32) -> i32 {
+    value * value
+}
+
+fn cube(value: i32) -> i32 {
+    value * value * value
 }
