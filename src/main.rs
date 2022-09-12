3e8f5dac1f09385e7f83d5563370a093a91bfbd1
Arun Fung <arunfung@gmail.com>
2022-09-12 18:10:06 +0800

    分号;表示其返回值为 unit

--- src/main.rs
+++ src/main.rs
@@ -46,19 +46,34 @@ fn main() {
     // fs::write(output, md.as_bytes()).unwrap();
 
     // 3、函数传参
-    println!("apply square: {}", apply(2, square));
-    println!("apply cube: {}", apply(2, cube));
+    // println!("apply square: {}", apply(2, square));
+    // println!("apply cube: {}", apply(2, cube));
 
+    let is_pi = pi();
+    let is_unit1 = not_pi();
+    let is_unit2 = {
+        pi();
+    };
+
+    println!("is_pi: {:?}, is_unit1: {:?}, is_unit2: {:?}", is_pi, is_unit1, is_unit2);
 }
 
-fn apply(value: i32, f:fn(i32) -> i32) -> i32 {
-    f(value)
+// fn apply(value: i32, f:fn(i32) -> i32) -> i32 {
+//     f(value)
+// }
+//
+// fn square(value: i32) -> i32 {
+//     value * value
+// }
+//
+// fn cube(value: i32) -> i32 {
+//     value * value * value
+// }
+
+fn pi() -> f64 {
+    3.1415926
 }
 
-fn square(value: i32) -> i32 {
-    value * value
-}
-
-fn cube(value: i32) -> i32 {
-    value * value * value
+fn not_pi() {
+    3.1415926;
 }
